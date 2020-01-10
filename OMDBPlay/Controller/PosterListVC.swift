2b7b1c1f744ffa35da434896d96da36a22e50faa
Rohit Saini <sketchlearn7@gmail.com>
2020-01-10 23:26:21 +0530

    first commit after creating project

---
+++ OMDBPlay/Controller/PosterListVC.swift
@@ -0,0 +1,30 @@
+//
+//  PosterListVC.swift
+//  OMDBPlay
+//
+//  Created by Rohit Saini on 10/01/20.
+//  Copyright © 2020 Rohit Saini. All rights reserved.
+//
+
+import UIKit
+
+class PosterListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
